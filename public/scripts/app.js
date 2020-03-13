35dd791dc972f2f8cfa9cf6b18df127355e3c862
Naitik Hingu <naitikhingu652@gmail.com>
2020-03-13 12:58:54 +1100

    moving from yarn to npm with babel 7 and local dependencies

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,6 @@
 "use strict";
 
 console.log("app.js is running!");
-
-var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
-);
+var template = React.createElement("h1", null, "Indecision App");
 var appRoot = document.getElementById("app");
-
 ReactDOM.render(template, appRoot);
