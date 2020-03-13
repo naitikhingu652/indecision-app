8f974bd55c051c498592321049a8ceb3b0d3c82f
Naitik Hingu <naitikhingu652@gmail.com>
2020-03-13 13:20:02 +1100

    Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log("app.js is running!");
-var template = React.createElement("h1", null, "Indecision App");
+var template = React.createElement("div", null, React.createElement("h1", null, "Indecision App"), React.createElement("p", null, "This is some info"), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
+var assignmentTemplate = React.createElement("div", null, React.createElement("h1", null, "Naitik Hingu"), React.createElement("p", null, "Age: 24"), React.createElement("p", null, "Location: Sydney"));
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+ReactDOM.render(assignmentTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 console.log("app.js is running!");
 
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var assignmentTemplate = (
+  <div>
+    <h1>Naitik Hingu</h1>
+    <p>Age: 24</p>
+    <p>Location: Sydney</p>
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(assignmentTemplate, appRoot);
