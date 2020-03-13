c33ef889c6bef6e651e16ddd21b48fd837211401
Naitik Hingu <naitikhingu652@gmail.com>
2020-03-13 12:43:39 +1100

    setting up babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
+"use strict";
+
 console.log("app.js is running!");
 
-var template = React.createElement("p", null, "This is JSX from app.js!");
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecision App"
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
