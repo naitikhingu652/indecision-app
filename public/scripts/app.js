bef6e2cb7e58daf6f4f9e9e35c7385fa953f309d
Naitik Hingu <naitikhingu652@gmail.com>
2020-03-13 11:24:51 +1100

    using sample jsx without babel

---
+++ public/scripts/app.js
@@ -0,0 +1,6 @@
+console.log("app.js is running!");
+
+var template = React.createElement("p", null, "This is JSX from app.js!");
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
