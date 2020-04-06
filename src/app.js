97b83edee2e6d557a98a552df8c29abf5d3a3568
Naitik Hingu <naitikhingu652@gmail.com>
2020-04-07 00:00:07 +1000

    ES6 Import and Export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+import { square, add } from './utils';
+import { isAdult, canDrink } from './person';
+
 console.log('app.js is running!');
+console.log(square(4));
+console.log(add(2,3));
+
+console.log(isAdult(17));
+console.log(canDrink(22));
---
+++ src/utils.js
@@ -0,0 +1,16 @@
+console.log('utils.js is running');
+
+export const square = (x) => x * x;
+
+export const add  = (a, b) => a + b;
+
+// const square = (x) => x * x;
+
+// const add  = (a, b) => a + b;
+
+// export {
+//   square,
+//   add
+// }
+
+
